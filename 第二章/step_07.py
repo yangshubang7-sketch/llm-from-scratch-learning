aa2b338240e3c2b9ca4befc1d379457392ab572c
Yanshov <2578903027@qq.com>
2026-04-07 20:32:24 +0800

    第二章  各部分 代码实现

--- 第二章/step_07.py
+++ 第二章/step_07.py
@@ -13,17 +13,23 @@ full_ids = tokenizer.encode(text)
 max_length = 4
 stride = 1
 # 3. 创建空列表 inputs 和 targets
-inputs , targets= []
+inputs = []
+targets= []
 # 4. 用 for 循环 i 从 0 到 len(full_ids)-max_length，步长为 stride：
 #    4.1 input_chunk = full_ids[i : i+max_length]
 #    4.2 target_chunk = full_ids[i+1 : i+max_length+1]
 #    4.3 inputs.append(input_chunk)
 #    4.4 targets.append(target_chunk)
-for i in range()
+for i in range(0 , len(full_ids) - max_length , stride):
+    input_chunk = full_ids[i: i + max_length]
+    target_chunk = full_ids[i + 1: i + max_length + 1]
+    inputs.append(input_chunk)
+    targets.append(target_chunk)
 # 5. 打印生成的样本数量
-
+print(len(inputs) , len(targets))
 # 6. 打印第一个样本的输入 ID、目标 ID，以及用 tokenizer.decode 解码后的文本
 #    例如：print("输入文本:", tokenizer.decode(inputs[0]))
 #          print("目标文本:", tokenizer.decode(targets[0]))
-
+print("输入文本:", tokenizer.decode(inputs[0]))
+print("目标文本:", tokenizer.decode(targets[0]))
 # 7. （选做）将上述逻辑封装成 PyTorch Dataset 类，参考书本 GPTDatasetV1
